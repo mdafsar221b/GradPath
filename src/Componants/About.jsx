9f36e650edbb9637798d1fa2fc28215c9f9bde7c
Md Afsar <afsarsayyyed@gmail.com>
2025-02-12 23:38:34 +0530

    Adds About and Haeder components

---
+++ src/Componants/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className="mt-10 bg-gray-900 p-6 rounded-lg w-full  shadow-lg">
+          <h3 className="text-xl font-semibold">About GradPath</h3>
+          <p className="text-gray-400 mt-2">
+            Empowering students with knowledge sharing
+          </p>
+          <p className="mt-4 text-gray-300">
+            GradPath is a comprehensive note-sharing platform designed to help students access and share academic resources across various engineering branches and semesters. With a wide range of subjects and an easy-to-navigate interface, we aim to make learning collaborative and efficient.
+          </p>
+        </div>
+  )
+}
+
+export default About
---
+++ src/Componants/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="w-full py-6">
+        <h1 className="text-3xl font-bold">GRAD-PATH</h1>
+      </header>
+  )
+}
+
+export default Header
