fd28b044ee711630eb99bda7bcb5f4ada1f2b60e
Md Afsar <afsarsayyyed@gmail.com>
2025-02-14 03:03:21 +0530

    made buttons interactive

--- src/Componants/Semester.jsx
+++ src/Componants/Semester.jsx
@@ -1,26 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react';
 import SemesterCard from './SemesterCard';
+import SemesterOne from './SemesterOne';
 
 const SemesterSection = () => {
-    const Semester = [
-  { name: " Sem - 1" },
-  { name: " Sem - 2" },
-  { name: " Sem - 3" },
-  { name: " Sem - 4" },
-  { name: " Sem - 5" },
-  { name: " Sem - 6" }
- 
-];
-  return (
-    <div>
-         <h3 className="text-2xl font-semibold mt-12">BCA DDU Semester</h3>
-        <div className="mt-6 flex items-center justify-center flex-wrap gap-6">
-          {Semester.map((Semester, index) => (
-            <SemesterCard Semester={Semester} key={index} />
-          ))}
-        </div>
-    </div>
-  )
-}
+    const [showSemesterOne, setShowSemesterOne] = useState(false);
 
-export default SemesterSection
+    const Semester = [
+        { name: " Sem - 1" },
+        { name: " Sem - 2" },
+        { name: " Sem - 3" },
+        { name: " Sem - 4" },
+        { name: " Sem - 5" },
+        { name: " Sem - 6" }
+    ];
+
+    const handleShowSemesterOne = () => {
+        setShowSemesterOne(true);
+    };
+
+    return (
+        <div>
+            <h3 className="text-2xl font-semibold mt-12">BCA DDU Semester</h3>
+            {showSemesterOne ? (
+                <SemesterOne />
+            ) : (
+                <div className="mt-6 flex items-center justify-center flex-wrap gap-6">
+                    {Semester.map((semester, index) => (
+                        <SemesterCard 
+                            Semester={semester} 
+                            key={index} 
+                            onClick={handleShowSemesterOne} 
+                        />
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default SemesterSection;
