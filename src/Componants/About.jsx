0248020c4006cb293f62b147b9a3452a95f085d6
Md Afsar <afsarsayyyed@gmail.com>
2025-03-04 21:23:54 +0530

    improved button Quality

--- src/Componants/About.jsx
+++ src/Componants/About.jsx
@@ -19,10 +19,16 @@ const About = () => {
        </p>
 
       </div>
-      <div className='flex sm:flex-none px-4 sm:px-6 sm:mt-6 py-2 bg-[#588157] text-white rounded-lg hover:bg-green-600 transition-colors duration-200 text-sm sm:text-base text-center gap-2 items-center justify-center '>
-        <a href="/src\assets\Resources\Syllabus.pdf" download> Download Syllabus</a>
-        <img src="/src/assets/download-icon.svg" alt=""  className='w-5'/>
-      </div>
+
+       <div className="mt-6">
+      <a className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition duration-150 ease-in-out gap-2" href="/src\assets\Resources\Syllabus.pdf" download>
+      Download Syllabus    
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" fill="none" className="w-5 h-5 mr-2 -ml-1">
+        <path d="M12 4v12m8-8l-8 8-8-8" strokeWidth={2} strokeLinejoin="round" strokeLinecap="round" />
+      </svg>
+    </a>
+
+       </div>
 
 
     </div>
